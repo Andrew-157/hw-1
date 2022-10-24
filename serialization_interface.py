8d0df5cb53aac4f1b4517b777607e763fc4f1f48
Andrew Subotin <subotinandrey5@gmail.com>
2022-10-24 16:00:04 +0300

    f

---
+++ serialization_interface.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+import pickle
+import json
+
+
+class SerializationInterface(metaclass=ABCMeta):
+
+    @abstractmethod
+    def serialize_data(self):
+        pass
+
+
+class SerializeJSON(SerializationInterface):
+
+    def serialize_data(self, data):
+        with open("json.json", "w") as file:
+            json.dump(data, file)
+
+
+class SerializeBinary(SerializationInterface):
+
+    def serialize_data(self, data):
+        with open("binary.bin", "wb") as file:
+            pickle.dump(data, file)
