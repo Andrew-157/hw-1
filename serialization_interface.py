2805ab2d82f246ecff4b5e7f05d3f5f7a15115bc
Andrew Subotin <subotinandrey5@gmail.com>
2022-10-26 09:03:11 +0300

    Update serialization_interface.py

--- serialization_interface.py
+++ serialization_interface.py
@@ -6,19 +6,25 @@ import json
 class SerializationInterface(metaclass=ABCMeta):
 
     @abstractmethod
-    def serialize_data(self, data):
+    def serialize_data(self):
         pass
 
 
 class SerializeJSON(SerializationInterface):
+    def __init__(self, data):
+        self.data = data
 
-    def serialize_data(self, data):
-        with open("json.json", "w") as file:
-            json.dump(data, file)
+    def serialize_data(self):
+       with open("json.json", "w") as file:
+           json.dump(self.data, file)
 
 
 class SerializeBinary(SerializationInterface):
+    def __init__(self, data):
+        self.data = data
 
-    def serialize_data(self, data):
+    def serialize_data(self):
         with open("binary.bin", "wb") as file:
-            pickle.dump(data, file)
+            pickle.dump(self.data, file)
+
+
